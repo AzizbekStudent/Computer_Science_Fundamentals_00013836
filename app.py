b3e32bb3003a87d719ee88521e3d5ae0a4e3b309
00013836_WIUT <113902046+AzizbekStudent@users.noreply.github.com>
2022-09-28 18:14:51 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from unicodedata import decimal
+
+decimal = int(input("Enter a decimal number: "))
+
+
+def decimal_to_octal(decimal):
+    pass
+
+
+print("The octal equivalent is:", decimal_to_octal(decimal))
+
+
+def decimal_to_octal(decimal):
+    octal = 0
+    i = 1
+    while (decimal != 0):
+        octal = octal + (decimal % 8) * i
+        decimal = int(decimal / 8)
+        i = i * 10
+    return octal
+
+    #the end
